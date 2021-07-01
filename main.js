b5c2e17ca4388afbe942275466f67626e95167c7
Vera Yeh <brisa8281@gmail.com>
2021-06-30 21:59:05 -0700

    Completed issue 3: user can start their car.

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
 var $body = document.querySelector('body');
 var $image = document.querySelector('img');
-var rotate = [
-  {
-    keyPressed: 'ArrowRight',
-    direction: 'right'
-  },
-  {
-    keyPressed: 'ArrowDown',
-    direction: 'down'
-  },
-  {
-    keyPressed: 'ArrowUp',
-    direction: 'up'
-  },
-  {
-    keyPressed: 'ArrowLeft',
-    direction: 'left'
+var $imageContainer = document.querySelector('.image-container');
+var car = {
+  directions: [
+    {
+      keyPressed: 'ArrowRight',
+      direction: 'right'
+    },
+    {
+      keyPressed: 'ArrowDown',
+      direction: 'down'
+    },
+    {
+      keyPressed: 'ArrowUp',
+      direction: 'up'
+    },
+    {
+      keyPressed: 'ArrowLeft',
+      direction: 'left'
+    }
+  ],
+  location: {
+    x: 0,
+    y: 0
   }
-];
+};
 
 $body.addEventListener('keydown', function (event) {
-  for (let i = 0; i < rotate.length; i++) {
-    if (event.key === rotate[i].keyPressed) {
-      $image.className = rotate[i].direction;
+  for (let i = 0; i < car.directions.length; i++) {
+    if (event.key === car.directions[i].keyPressed) {
+      $image.className = car.directions[i].direction;
     }
   }
 });
+
+$body.addEventListener('keydown', function (event) {
+  if (event.key === ' ') {
+    setInterval(start, 16);
+  }
+});
+
+function start() {
+  car.location.x += 12;
+  $imageContainer.style.left = car.location.x + 'px';
+}
